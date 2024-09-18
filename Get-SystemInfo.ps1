143503a2693a0b7f0836b882870a3a2d60c387fa
VirtualOx <37578273+virtualox@users.noreply.github.com>
2024-09-18 15:49:19 +0200

    Switching to Get-CimInstance
    By switching to Get-CimInstance from Get-WmiObject to ensure the script remains compatible with future versions of PowerShell and Windows.

--- Get-SystemInfo.ps1
+++ Get-SystemInfo.ps1
@@ -13,15 +13,30 @@ assess system details.
 https://github.com/virtualox/Get-SystemInfo.ps1
 #>
 
+<# 
+.SYNOPSIS
+This script gathers and prints various system information such as system model, motherboard manufacturer, 
+operating system and version, BIOS serial number, system SKU, installed memory, processor model and speed, 
+and hard drive information.
+
+.DESCRIPTION
+The script uses CIM (Common Information Model) to gather system information. It then prints the 
+information in a clear and easy-to-read format. It's useful for system administrators who need to quickly 
+assess system details.
+
+.LINK
+https://github.com/virtualox/Get-SystemInfo.ps1
+#>
+
 # Gather the data
-$computerSystem = Get-WmiObject -Class:Win32_ComputerSystem
-$baseBoard = Get-WmiObject -Class:Win32_BaseBoard
-$operatingSystem = Get-WmiObject -Class:Win32_OperatingSystem
-$physicalMemory = Get-WmiObject -Class:Win32_PhysicalMemory
-$processor = Get-WmiObject -Class:Win32_Processor
-$diskDrive = Get-WmiObject -Class:Win32_DiskDrive
-$BIOSSerialNumber = (Get-WmiObject -Class:Win32_BIOS).SerialNumber
-$systemSKU = (Get-WmiObject -Class:Win32_ComputerSystem).SystemSKUNumber
+$computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem
+$baseBoard = Get-CimInstance -ClassName Win32_BaseBoard
+$operatingSystem = Get-CimInstance -ClassName Win32_OperatingSystem
+$physicalMemory = Get-CimInstance -ClassName Win32_PhysicalMemory
+$processor = Get-CimInstance -ClassName Win32_Processor
+$diskDrive = Get-CimInstance -ClassName Win32_DiskDrive
+$BIOSSerialNumber = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
+$systemSKU = (Get-CimInstance -ClassName Win32_ComputerSystem).SystemSKUNumber
 
 # Print the data in a readable format
 Write-Host "System Model: $($computerSystem.Model)"
